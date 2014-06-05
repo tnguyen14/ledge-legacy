d81bd18204a24f0e0e913c04be83e4af5867bd59
Tri Nguyen <tri@tridnguyen.com>
2014-06-05 19:08:27 -0400

    get some data from server

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('\'Allo \'Allo!');
+jQuery(document).ready(function($) {
+    $.ajax({
+        url: 'http://localhost:3000/accounts/toan',
+        success: function(data) {
+            var balance = data.starting_balance;
+            $('.account-name').html(data.name);
+
+            _.each(data.transactions, function(tx) {
+                var html = '<tr>';
+                html += '<td>' + tx.date + '</td>';
+                html += '<td>' + tx.description + '</td>';
+                html += '<td>' + tx.amount + '</td>';
+                html += '<td>' + tx.category + '</td>';
+                html += '</tr>';
+                balance -= tx.amount;
+                $('.transactions').append(html);
+            });
+            $('.balance').append(balance);
+        }
+    })
+});
