1917823e0dbb18bf00a5b297edd925a89b55d802
Tri Nguyen <tri@tridnguyen.com>
2014-08-08 01:04:40 -0400

    simple section navigation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,4 +6,20 @@ var budget = require('./budget'),
 jQuery(document).ready(function($) {
   // default to budget
   budget.init();
+
+  // simple routing based on menu navigation
+  $('.nav li').on('click', function (e) {
+    var $this = $(e.delegateTarget),
+      section = $this.data('section');
+    $this.addClass('active');
+    $this.siblings('.active').removeClass('active');
+    switch (section) {
+      case 'checking':
+        checking.init();
+        break;
+      case 'budget':
+        budget.init();
+        break;
+    }
+  });
 });
