67466b03c3468d96ac9585ae46d8af9835588e18
Tri Nguyen <tri@tridnguyen.com>
2014-08-27 18:43:57 -0400

    use restify instead

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var connect = require('connect');
-var serveStatic = require('serve-static');
-var compression = require('compression');
+var restify = require('restify');
 
-var app = connect();
+var server = restify.createServer({
+  name: 'ledge-client',
+  version: '0.0.1'
+});
 
-app.use(compression());
-app.use(serveStatic('dist/'));
-app.listen(80);
+server.use(restify.gzipResponse());
+server.get(/.*/, serveStatic('dist/'));
+server.listen(process.env.PORT || 4000, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
